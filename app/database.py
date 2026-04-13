c488ab1d8ec79d208e22eab4f666affac55a036b
hayasaaki <zoticnichita@gmail.com>
2026-04-13 23:09:07 +0300

    docker upgrade

--- app/database.py
+++ app/database.py
@@ -1,11 +1,8 @@
-import time
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from psycopg.rows import dict_row
 from .config import settings 
-import psycopg2
 
 SQLALCHEMY_DATABASE_URL  = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
